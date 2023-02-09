96eaa5afc29e72f7f28490a860e0c36b76c6499a
Brian Ketelsen <bketelsen@gmail.com>
2023-02-09 12:21:54 -0500

    first post

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+    description = "My Home Manager Flake";
+
+    inputs = {
+        nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+        home-manager = {
+            url = "github:nix-community/home-manager";
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
+    };
+
+    outputs = {nixpkgs, home-manager, ...}: {
+        defaultPackage.x86_64-linux = home-manager.defaultPackage.x86_64-linux;
+
+        homeConfigurations = {
+            "bjk" = home-manager.lib.homeManagerConfiguration {
+                # Note: I am sure this could be done better with flake-utils or something
+                pkgs = nixpkgs.legacyPackages.x86_64-darwin;
+
+                modules = [ ./home.nix ];
+            };
+        };
+    };
+}
---
+++ home.nix
@@ -0,0 +1,10 @@
+{pkgs, ...}: {
+    home.username = "bjk";
+    home.homeDirectory = "/home/bjk";
+    home.packages = [
+        pkgs.nixfmt
+        pkgs.cowsay
+    ];
+    home.stateVersion = "22.11"; # To figure this out (in-case it changes) you can comment out the line and see what version it expected.
+    programs.home-manager.enable = true;
+}
