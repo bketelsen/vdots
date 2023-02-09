01cf68e9a87a8e2c01ca94a5f341e05cd005d861
Brian Ketelsen <bketelsen@gmail.com>
2023-02-09 13:04:37 -0500

    starship

--- home.nix
+++ home.nix
@@ -3,11 +3,15 @@
     home.homeDirectory = "/home/bjk";
     home.packages = [
         pkgs.nixfmt
+	    pkgs.neovim
         pkgs.cowsay
         pkgs.fzf
         pkgs.lazygit
         pkgs.ripgrep
     ];
+    programs.direnv.enable = true;
+    programs.starship.enable = true;
+    services.syncthing.enable = true;
     home.stateVersion = "22.11"; # To figure this out (in-case it changes) you can comment out the line and see what version it expected.
     programs.home-manager.enable = true;
 }
