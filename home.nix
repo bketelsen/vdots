f9439751f63f3e4754c71c8a680fc039ee6467b6
Brian Ketelsen <bketelsen@gmail.com>
2023-02-10 20:40:26 -0500

    who knows

--- home.nix
+++ home.nix
@@ -1,4 +1,4 @@
-{pkgs, ...}: {
+{pkgs, misc,...}: {
     home.username = "bjk";
     home.homeDirectory = "/home/bjk";
     home.packages = [
@@ -9,12 +9,47 @@
         pkgs.lazygit
         pkgs.ripgrep
         pkgs.jq
+        pkgs.dive
     ];
-    programs.bash.enable = true;
-    programs.bash.profileExtra = ". /home/bjk/.nix-profile/etc/profile.d/nix.sh";
+    programs.bash = {
+        enable = true;
+        profileExtra = ". /home/bjk/.nix-profile/etc/profile.d/nix.sh";
+        enableCompletion = true;
+        enableVteIntegration = true;
+    };
     programs.direnv.enable = true;
     programs.starship.enable = true;
+    programs.atuin.enable = true;
+    programs.fzf.enable = true;
+    programs.gh.enable = true;
+    programs.git = {
+        enable = true;
+        userEmail = "bketelsen@gmail.com";
+        userName = "Brian Ketelsen";
+    };
+    programs.zellij = {
+        enable = true;
+    };
+    dconf.enable = true;
     services.syncthing.enable = true;
+    home.shellAliases = {
+      npu = "export NIXPKGS_ALLOW_UNFREE=1; nix profile upgrade --impure '.*'";
+      hmh = "home-manager-help";
+    };
+    home.sessionPath = [
+        "$HOME/bin"
+        "$HOME/.local/bin"
+    ];
+    gtk.theme= {
+        name = "Adwaita";
+        package = "pkgs.gnome.gnome-themes-extra";
+    };
+    gtk.iconTheme = {
+        package = "pkgs.gnome.adwaita-icon-theme";
+        name = "Adwaita";
+    };
+    fonts.fontconfig.enable = true;
+    manual.html.enable = true;
     home.stateVersion = "22.11"; # To figure this out (in-case it changes) you can comment out the line and see what version it expected.
     programs.home-manager.enable = true;
 }
