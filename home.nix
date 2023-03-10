3e468f3045c1535b1f7a41b824ff430a166d538d
Brian Ketelsen <bketelsen@gmail.com>
2023-03-10 10:04:05 -0500

    cleanup

--- home.nix
+++ home.nix
@@ -48,7 +48,6 @@
         name = "Adwaita";
     };
     fonts.fontconfig.enable = true;
-    manual.html.enable = true;
     home.stateVersion = "22.11"; # To figure this out (in-case it changes) you can comment out the line and see what version it expected.
     programs.home-manager.enable = true;
 }
