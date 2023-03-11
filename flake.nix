8a724231278ab56f9f520d6e863043ec254c8c0d
Brian Ketelsen <bketelsen@gmail.com>
2023-03-10 19:49:59 -0500

    format

--- flake.nix
+++ flake.nix
@@ -1,25 +1,25 @@
 {
-    description = "My Home Manager Flake";
+  description = "My Home Manager Flake";
 
-    inputs = {
-        nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-        home-manager = {
-            url = "github:nix-community/home-manager";
-            inputs.nixpkgs.follows = "nixpkgs";
-        };
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
+  };
 
-    outputs = {nixpkgs, home-manager, ...}: {
-        defaultPackage.x86_64-linux = home-manager.defaultPackage.x86_64-linux;
-	home-manager.useGlobalPkgs = true;
-	nixpkgs.config.allowUnfree = true;
-        homeConfigurations = {
-            "bjk" = home-manager.lib.homeManagerConfiguration {
-                # Note: I am sure this could be done better with flake-utils or something
-                pkgs = nixpkgs.legacyPackages.x86_64-linux;
+  outputs = { nixpkgs, home-manager, ... }: {
+    defaultPackage.x86_64-linux = home-manager.defaultPackage.x86_64-linux;
+    home-manager.useGlobalPkgs = true;
+    nixpkgs.config.allowUnfree = true;
+    homeConfigurations = {
+      "bjk" = home-manager.lib.homeManagerConfiguration {
+        # Note: I am sure this could be done better with flake-utils or something
+        pkgs = nixpkgs.legacyPackages.x86_64-linux;
 
-                modules = [ ./home.nix ];
-            };
-        };
+        modules = [ ./home.nix ];
+      };
     };
+  };
 }
--- home.nix
+++ home.nix
@@ -1,5 +1,5 @@
-{pkgs, misc,...}: {
-nixpkgs = {
+{ pkgs, misc, ... }: {
+  nixpkgs = {
     # You can add overlays here
     overlays = [
       # If you want to use overlays exported from other flakes:
@@ -20,58 +20,54 @@ nixpkgs = {
       allowUnfreePredicate = (_: true);
     };
   };
-    home.username = "bjk";
-    home.homeDirectory = "/home/bjk";
-    home.packages = [
-	pkgs.nixfmt
-	pkgs.neovim
-	pkgs.fzf
-	pkgs.lazygit
-	pkgs.ripgrep
-	pkgs.jq
-	pkgs.dive
-	pkgs.htop
-	pkgs.yq
-	pkgs.vscode
-    ];
-    programs.bash = {
-        enable = true;
-        profileExtra = ". /home/bjk/.nix-profile/etc/profile.d/nix.sh";
-        enableCompletion = true;
-        enableVteIntegration = true;
-    };
-    programs.direnv.enable = true;
-    programs.starship.enable = true;
-    programs.atuin.enable = true;
-    programs.fzf.enable = true;
-    programs.gh.enable = true;
-    programs.git = {
-        enable = true;
-        userEmail = "bketelsen@gmail.com";
-        userName = "Brian Ketelsen";
-    };
-    programs.zellij = {
-        enable = true;
-    };
-    dconf.enable = true;
-    services.syncthing.enable = true;
-    home.shellAliases = {
-      npu = "export NIXPKGS_ALLOW_UNFREE=1; nix profile upgrade --impure '.*'";
-      hmh = "home-manager-help";
-    };
-    home.sessionPath = [
-        "$HOME/bin"
-        "$HOME/.local/bin"
-    ];
-    gtk.theme= {
-        name = "Adwaita";
-        package = "pkgs.gnome.gnome-themes-extra";
-    };
-    gtk.iconTheme = {
-        package = "pkgs.gnome.adwaita-icon-theme";
-        name = "Adwaita";
-    };
-    fonts.fontconfig.enable = true;
-    home.stateVersion = "22.11"; # To figure this out (in-case it changes) you can comment out the line and see what version it expected.
-    programs.home-manager.enable = true;
+  home.username = "bjk";
+  home.homeDirectory = "/home/bjk";
+  home.packages = [
+    pkgs.nixfmt
+    pkgs.neovim
+    pkgs.fzf
+    pkgs.lazygit
+    pkgs.ripgrep
+    pkgs.jq
+    pkgs.dive
+    pkgs.htop
+    pkgs.yq
+    pkgs.vscode
+  ];
+  programs.bash = {
+    enable = true;
+    profileExtra = ". /home/bjk/.nix-profile/etc/profile.d/nix.sh";
+    enableCompletion = true;
+    enableVteIntegration = true;
+  };
+  programs.direnv.enable = true;
+  programs.starship.enable = true;
+  programs.atuin.enable = true;
+  programs.fzf.enable = true;
+  programs.gh.enable = true;
+  programs.git = {
+    enable = true;
+    userEmail = "bketelsen@gmail.com";
+    userName = "Brian Ketelsen";
+  };
+  programs.zellij = { enable = true; };
+  dconf.enable = true;
+  services.syncthing.enable = true;
+  home.shellAliases = {
+    npu = "export NIXPKGS_ALLOW_UNFREE=1; nix profile upgrade --impure '.*'";
+    hmh = "home-manager-help";
+  };
+  home.sessionPath = [ "$HOME/bin" "$HOME/.local/bin" ];
+  gtk.theme = {
+    name = "Adwaita";
+    package = "pkgs.gnome.gnome-themes-extra";
+  };
+  gtk.iconTheme = {
+    package = "pkgs.gnome.adwaita-icon-theme";
+    name = "Adwaita";
+  };
+  fonts.fontconfig.enable = true;
+  home.stateVersion =
+    "22.11"; # To figure this out (in-case it changes) you can comment out the line and see what version it expected.
+  programs.home-manager.enable = true;
 }
